cbae94f33f037281f45f670662ebb73e76f230a1
conor-smith <conormartindale@gmail.com>
2022-02-20 15:23:17 +1100

    Set up database. Removed files

--- BakeOff/apps.py
+++ BakeOff/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BakeoffConfig(AppConfig):
+class BakeOffConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'BakeOff'
